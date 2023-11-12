e79a780c8410eebc4afcf2b513c85593703f749b
Carla Diniz <carladiniz@gmail.com>
2023-11-12 14:55:22 -0300

    12-11

---
+++ script/script.js
@@ -0,0 +1,11 @@
+function showSection(sectionId) {
+    var sections = document.querySelectorAll('.tm-section');
+    sections.forEach(function(section) {
+        section.style.display = 'none';
+    });
+
+    var selectedSection = document.getElementById(sectionId);
+    if (selectedSection) {
+        selectedSection.style.display = 'block';
+    }
+}
